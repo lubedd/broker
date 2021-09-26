84faa3878d7d52c950ed17f45f98a3413bc4c684
Nikolay Korolev <lubeddd@gmail.com>
2021-09-26 14:28:37 +1000

    :globe_with_meridians: Second commit

--- cmd/broker/main.go
+++ cmd/broker/main.go
@@ -2,6 +2,12 @@ package main
 
 import "broker/broker/internal/app"
 
+// TODO: Парсер json в задачи
+// TODO: Exchange type (Funout, Topic)
+// TODO: Durable queue, exchange
+// TODO: Dead letters pool
+// TODO: Web интерфейс с профилированием брокера
+
 func main() {
 	app.Run()
 }
